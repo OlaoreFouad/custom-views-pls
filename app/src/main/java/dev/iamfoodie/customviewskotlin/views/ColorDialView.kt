863003499ba580a8750d8897a9a68358a99a9dd1
Foodie <olaorefouad565@gmail.com>
2020-04-12 21:39:17 +0100

    drew tick marks around the drawable, added the no color drawable too, I also used the onMEasure method to make sure the view reacted to changes in the view. Some custom attributes were also added though. I am done with this course, the concepts got complex and were hardly explained to my satisfaction, so it is time to find a better alternative

--- app/src/main/java/dev/iamfoodie/customviewskotlin/views/ColorDialView.kt
+++ app/src/main/java/dev/iamfoodie/customviewskotlin/views/ColorDialView.kt
@@ -16,20 +16,78 @@ class ColorDialView @JvmOverloads
     constructor(private val ctx: Context, private val attributeSet: AttributeSet? = null, private val defStyle: Int = 0)
     : View(ctx, attributeSet, defStyle) {
 
+    private var colors = arrayListOf<Int>(
+        Color.RED, Color.YELLOW, Color.DKGRAY, Color.BLACK, Color.BLUE, Color.GRAY, Color.GREEN, Color.MAGENTA
+    )
+
     private var dialDrawable: Drawable? = null
     private var dialDiameter = toDP(100)
+    private var noColorDrawable: Drawable? = null
 
     private var horizontalSize = 0f
     private var verticalSize = 0f
 
+//    Padding Values
+    private var extraPadding = toDP(30).toFloat()
+    private var totalLeftPadding = 0f
+    private var totalRightPadding = 0f
+    private var totalTopPadding = 0f
+    private var totalBottomPadding = 0f
+
+    private var paint = Paint().also {
+        it.color = Color.BLUE
+        it.isAntiAlias = true
+    }
+
+//    Tick size
+    private var tickSize = toDP(10).toFloat()
+    private var tickPositionVertical = 0f
+
     private var centerHorizontal = 0f
     private var centerVertical = 0f
 
+    private var angleBetweenColors = 0f
+
     init {
+
+        val typedArray = ctx.obtainStyledAttributes(attributeSet, R.styleable.ColorDialView)
+
+        try {
+
+            val customColors = typedArray.getTextArray(R.styleable.ColorDialView_colors)
+                .map { Color.parseColor(it.toString()) }
+                as ArrayList<Int>?
+
+            customColors?.let {
+                colors = customColors
+            }
+
+            dialDiameter = typedArray.getDimension(
+                R.styleable.ColorDialView_dialDiameter, toDP(100).toFloat()
+            ).toInt()
+
+            tickSize = typedArray.getDimension(
+                R.styleable.ColorDialView_tickRadius, toDP(10).toFloat()
+            ).toFloat()
+
+            extraPadding = typedArray.getDimension(
+                R.styleable.ColorDialView_extraPadding, toDP(30).toFloat()
+            )
+
+
+        } finally {
+            typedArray.recycle()
+        }
+
         dialDrawable = ctx.getDrawable(R.drawable.ic_dial)
         dialDrawable?.let {
             it.bounds = getCenteredBounds(dialDiameter)
         }
+        noColorDrawable = ctx.getDrawable(R.drawable.ic_no_color).also {
+            it?.bounds = getCenteredBounds(tickSize.toInt(), 2)
+        }
+        colors[0] = android.R.color.transparent
+        angleBetweenColors = 360f / colors.size
 
         refreshValues()
     }
@@ -40,11 +98,19 @@ class ColorDialView @JvmOverloads
     }
 
     private fun refreshValues() {
-        horizontalSize = dialDiameter.toFloat()
-        verticalSize = dialDiameter.toFloat()
 
-        centerHorizontal = (horizontalSize / 2f).toFloat()
-        centerVertical = (verticalSize / 2f).toFloat()
+        totalLeftPadding = paddingLeft + extraPadding
+        totalRightPadding = paddingRight + extraPadding
+        totalTopPadding = paddingTop + extraPadding
+        totalBottomPadding = paddingBottom + extraPadding
+
+        tickPositionVertical = paddingTop + extraPadding / 2f
+
+        horizontalSize = totalLeftPadding + totalRightPadding + dialDiameter.toFloat()
+        verticalSize = totalTopPadding + totalBottomPadding + dialDiameter.toFloat()
+
+        centerHorizontal = totalLeftPadding + (horizontalSize - totalLeftPadding - totalRightPadding) / 2f
+        centerVertical = totalTopPadding + (verticalSize - totalTopPadding - totalBottomPadding) / 2f
     }
 
     private fun toDP(value: Int): Int {
@@ -55,8 +121,34 @@ class ColorDialView @JvmOverloads
         ).toInt()
     }
 
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val width = resolveSizeAndState(
+            horizontalSize.toInt(), widthMeasureSpec, 0
+        )
+
+        val height = resolveSizeAndState(
+            verticalSize.toInt(), heightMeasureSpec, 0
+        )
+
+        setMeasuredDimension(width, height)
+    }
+
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+        var saveCount = canvas.save()
+        colors.forEachIndexed { i, color ->
+
+            if (i == 0) {
+                canvas.translate(centerHorizontal, tickPositionVertical)
+                noColorDrawable?.draw(canvas)
+                canvas.translate(-centerHorizontal, -tickPositionVertical)
+            } else {
+                paint.color = color
+                canvas.drawCircle(centerHorizontal, tickPositionVertical, tickSize, paint)
+            }
+            canvas.rotate(angleBetweenColors, centerHorizontal, centerVertical)
+        }
+        canvas.restoreToCount(saveCount)
         canvas.translate(centerHorizontal, centerVertical)
         dialDrawable?.draw(canvas)
     }
